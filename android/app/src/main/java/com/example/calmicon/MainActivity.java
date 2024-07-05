e0087b6a1adda21a0dc5a33d476058a8a01df186
hrvyCased <salinas.johnharvey11@gmail.com>
2024-07-05 18:41:35 +0800

    Reminder for editing .java files.

--- android/app/src/main/java/com/example/calmicon/MainActivity.java
+++ android/app/src/main/java/com/example/calmicon/MainActivity.java
@@ -3,4 +3,5 @@ package com.example.calmicon;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    //Open this to edit the java code for Android project directory.
 }
